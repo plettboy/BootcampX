a56bae6e40aec16394543e327fb5e36dd2937832
plettboy <ryan.plett@yahoo.com>
2022-09-22 22:24:22 +0000

    parametrized querying added to protect agaisnt sql injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+const userInput = process.argv.slice(2);
+const cohortArg = userInput[0];
+const limitArg = userInput[1];
+const queryString = `
+SELECT students.id, students.name, cohorts.name as cohort_name
 FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2
+`;
+const values = [`%${cohortArg}%`, limitArg];
+
+pool.query(queryString, values)
 .then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
+  console.log(res.rows);
+})
+.catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,3 +1,5 @@
+
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
